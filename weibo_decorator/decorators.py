356579f373be890ad578efdb8ca86989a6c1e89b
ResolveWang <1796246076@qq.com>
2016-07-21 22:01:23 +0800

    部分bug

--- weibo_decorator/decorators.py
+++ weibo_decorator/decorators.py
@@ -23,9 +23,9 @@ def save_decorator(func):
 # 用于超时设置
 def timeout_decorator(func):
     @wraps(func)
-    def time_limit(session, url, headers, verify):
+    def time_limit(session, url, headers):
         try:
-            return func(session, url, headers, verify)
+            return func(session, url, headers)
         except Exception as e:
             print('抓取{url}超时'.format(url=url))
             logging.error('抓取{url}失败，具体错误信息为{e},堆栈为{stack}'.format(url=url, e=e,
