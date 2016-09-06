31419d1778ff0fdce8ed5112db715ccc155a0ccd
w1796246076 <1796246076@qq.com>
2016-09-06 20:14:10 +0800

    当前微博开始扩散分析

--- task/get_repost.py
+++ task/get_repost.py
@@ -133,7 +133,99 @@ def _get_reposts(url, session, weibo_mid):
 
     else:
         logging.info('{url}为404页面'.format(url=url))
-        print('该微博{url}的源微博已经被删除了'.format(url=url))
+        print('该微博{url}已经被删除了'.format(url=url))
+
+
+def _get_current_reposts(url, session, weibo_mid):
+    """
+    修改过后的抓取主程序，由于微博频率限制比较严格，目前只抓取当前微博及其子微博，不抓取源微波
+    :param url:
+    :param session:
+    :return:
+    """
+    spread_other_caches = []
+    spread_others = []
+    spread_other_and_caches = []
+
+    html = get_page(url, session, headers)
+    reposts_count = status_parse.get_repostcounts(html)
+    # 更新weibo_search_data表中的转发数、评论数
+    weibosearch_dao.update_weibo_repost(weibo_mid, reposts_count)
+
+    if not basic.is_404(html):
+        root_url = url
+        mid = status_parse.get_mid(html)
+        user_id = status_parse.get_userid(html)
+        user_name = status_parse.get_username(html)
+        post_time = status_parse.get_statustime(html)
+        device = status_parse.get_statussource(html)
+        comments_count = status_parse.get_commentcounts(html)
+        reposts_count = status_parse.get_repostcounts(html)
+        root_user = get_userinfo.get_profile(user_id, session, headers)
+
+        rs = spread_original_dao.save(root_user, mid, post_time, device, reposts_count, comments_count, root_url)
+
+        if rs is False:
+            print('源微博的扩散信息已经获取过了')
+            return
+
+        print('转发数为{counts}'.format(counts=reposts_count))
+
+        if reposts_count > 0:
+            base_url = 'http://weibo.com/aj/v6/mblog/info/big?ajwvr=6&id={mid}&page={currpage}'
+            soc = SpreadOtherCache()
+            soc.set_id(user_id)
+            soc.set_name(user_name)
+            spread_other_caches.append(soc)
+            page = 1
+            ajax_url = base_url.format(mid=mid, currpage=page)
+            source = get_page(ajax_url, session, headers, False)
+            print('本次转发信息url为：' + ajax_url)
+
+            repost_json = json.loads(source)
+            total_page = int(repost_json['data']['page']['totalpage'])
+            page = total_page
+            page_counter = 0
+            while page > 0 and page_counter < page_max:
+                ajax_url = base_url.format(mid=mid, currpage=page)
+                repost_info = session.get(ajax_url).text
+                repost_json = json.loads(repost_info)
+                repost_html = repost_json['data']['html']
+                repost_urls = status_parse.get_reposturls(repost_html)
+
+                for repost_url in repost_urls:
+                    repost_cont = get_statusinfo.get_status_info(repost_url, session, user_id, user_name, headers)
+
+                    if repost_cont is not None:
+                        spread_other_and_caches.append(repost_cont)
+
+                for soac in spread_other_and_caches:
+                    if soac.get_so().id != '':
+                        spread_others.append(soac.get_so())
+                        spread_other_caches.append(soac.get_soc())
+                print('当前位于第{currpage}页'.format(currpage=page))
+                page -= 1
+                page_counter += 1
+
+            for so in spread_others:
+                for i in spread_other_caches:
+                    if so.upper_user_name == i.get_name():
+                        so.upper_user_id = i.get_id()
+                        break
+                    if so.verify_type == '':
+                        so.verify_type = 0
+                    else:
+                        so.upper_user_id = user_id
+
+            # todo 找出数据重复的原因
+            spread_others = list(set(spread_others))
+
+            spread_other_dao.save(spread_others)
+            print('一共获取了{num}条转发信息'.format(num=len(spread_others)))
+            print('该条微博的转发信息已经采集完成')
+    else:
+        logging.info('{url}为404页面'.format(url=url))
+        print('该微博{url}已经被删除了'.format(url=url))
 
 
 def get_all(d):
@@ -147,6 +239,6 @@ def get_all(d):
         # session放在里面是为了防止某个抓取队列太长或者转发微博太多
         session = d['session']
         logging.info('正在抓取url为{url}的微博'.format(url=data['url']))
-        _get_reposts(data['url'], session, data['mid'])
+        _get_current_reposts(data['url'], session, data['mid'])
         weibosearch_dao.update_weibo_url(data['mid'])
     logging.info('本次启动一共抓取了{count}个页面'.format(count=count))
