da13564bdadb9ebacc91b8d07f1a101c2b3c4711
resolvewang <1796246076@qq.com>
2017-03-01 16:58:52 +0800

    完善上下文管理器，增加数据库操作单元测试

--- weibo_decorator/decorators.py
+++ weibo_decorator/decorators.py
@@ -5,19 +5,6 @@ from utils.util_cls import Timeout, KThread
 from logger.log import storage, parser, crawler
 
 
-# 用于捕捉插入数据时的异常
-def save_decorator(func):
-    @wraps(func)
-    def save_process(*args):
-        try:
-            func(*args)
-        except Exception as e:
-            for i in args:
-                storage.error('未成功插入的对象属性:{i}'.format(i=i.__dict__))
-            storage.error('插入失败，具体错误信息为{e},堆栈为{stack}'.format(e=e, stack=format_tb(e.__traceback__)[0]))
-    return save_process
-
-
 # 用于超时设置
 def timeout_decorator(func):
     @wraps(func)
@@ -28,6 +15,7 @@ def timeout_decorator(func):
             crawler.error('抓取{url}失败，具体错误信息为{e},堆栈为{stack}'.format(url=url, e=e,
                                                                    stack=format_tb(e.__traceback__)[0]))
             return None
+
     return time_limit
 
 
@@ -53,27 +41,10 @@ def parse_decorator(return_type):
                     return ''
                 else:
                     return 0
+
         return handle_error
+
     return page_parse
-
-
-# 用于捕捉数据库操作时的异常
-def dbtimeout_decorator(return_type):
-    def connect_process(func):
-        @wraps(func)
-        def handle_error(*keys):
-            try:
-                return func(*keys)
-            except Exception as e:
-                storage.error(e)
-                if return_type == 2:
-                    return []
-                elif return_type == 1:
-                    return None
-                else:
-                    return False
-        return handle_error
-    return connect_process
 
 
 # 即使在抓取的时候设置了超时函数，抓取函数还是可能会超时，这是对get_page超时的完善
