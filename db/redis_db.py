eb608cc30189ecef253c1b765339b07d4159d4f9
resolvewang <1796246076@qq.com>
2017-05-01 20:10:28 +0800

    将redis相关操作放到一个模块下

---
+++ db/redis_db.py
@@ -0,0 +1,34 @@
+import json
+import redis
+from config.conf import get_redis_args
+
+
+redis_args = get_redis_args()
+
+
+class Cookies(object):
+    rd_con = redis.StrictRedis(host=redis_args.get('host'), port=redis_args.get('port'), db=redis_args.get('cookies'))
+
+    @classmethod
+    def store_cookies(cls, name, cookies):
+        pickled_cookies = json.dumps(cookies)
+        cls.rd_con.set(name, pickled_cookies)
+        # 为cookie设置过期时间，防止某些账号登录失败，还会获取到失效cookie
+        cls.rd_con.expire(name, 20 * 60 * 60)
+
+    @classmethod
+    def fetch_cookies(cls):
+        random_name = cls.rd_con.randomkey()
+        return random_name, json.loads(cls.rd_con.get(random_name).decode('utf-8'))
+
+    @classmethod
+    def delete_cookies(cls, name):
+        cls.rd_con.delete(name)
+
+
+class Urls(object):
+    rd_con = redis.StrictRedis(host=redis_args.get('host'), port=redis_args.get('port'), db=redis_args.get('urls'))
+
+    @classmethod
+    def store_crawl_url(cls, url, result):
+        cls.rd_con.set(url, result)
