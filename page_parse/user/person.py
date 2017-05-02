0f80d86a957c84a289cf12b7d988eee0339c1dec
resolvewang <1796246076@qq.com>
2017-05-02 14:57:48 +0800

    修复解析完成之前就提前返回的bug

--- page_parse/user/person.py
+++ page_parse/user/person.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 from page_parse.user import public
 from decorator.decorators import parse_decorator
-from entities.user import User
+from db.models import User
 
 
 @parse_decorator(0)
@@ -60,11 +60,10 @@ def get_detail(html):
     user = User()
     cont = public.get_right(html)
     if cont == '':
-        return user
+        return None
     soup = BeautifulSoup(cont, 'html.parser')
     basic_modules = soup.find_all(attrs={'class': 'WB_cardwrap S_bg2'})
     basic_info = soup.find_all(attrs={'class': 'li_1 clearfix'})
-
     for each_module in basic_modules:
         try:
             basic_str = each_module.find(attrs={'class': 'main_title W_fb W_f14'}).get_text()
@@ -72,63 +71,39 @@ def get_detail(html):
                 for each in basic_info:
                     each_str = each.get_text()
                     if '昵称' in each_str:
-                        nickname = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.screen_name = nickname
+                        user.name = each.find(attrs={'class': 'pt_detail'}).get_text()
                     elif '所在地' in each_str:
-                        location = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.location = location
-                        if ' ' in location:
-                            province = location.split(' ')[0]
-                            city = location.split(' ')[1]
-                            user.province = province
-                            user.city = city
-                        else:
-                            user.province = location
-                            user.city = ''
+                        user.location = each.find(attrs={'class': 'pt_detail'}).get_text()
                     elif '性别' in each_str:
                         gender = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.gender = gender
-                    elif '性取向' in each_str:
-                        gender_prefer = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.gender_prefer = gender_prefer
-                    # elif '感情状况' in each_str:
-                    #     loving = each.find(attrs={'class': 'pt_detail'}).get_text()
+                        if gender == '男':
+                            user.gender = 1
+                        elif gender == '女':
+                            user.gender = 2
+                        else:
+                            user.gender = 0
                     elif '生日' in each_str:
-                        birthday = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.birthday = birthday
-                    elif '血型' in each_str:
-                        blood_type = each.find(attrs={'class': 'pt_detail'}).get_text()
-                        user.blood_type = blood_type
-                    elif '博客' in each_str:
-                        blog_url = each.find('a').get_text()
-                        user.blog_url = blog_url
+                        user.birthday = each.find(attrs={'class': 'pt_detail'}).get_text()
                     elif '简介' in each_str:
                         description = each.find(attrs={'class': 'pt_detail'}).get_text()
                         user.description = description.encode('gbk', 'ignore').decode('gbk')
                     elif '注册时间' in each_str:
-                        register_time = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\t', '').replace(
+                        user.register_time = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\t', '').replace(
                             '\r\n', '')
-                        user.register_time = register_time
-                    elif '个性域名' in each_str:
-                        personal_domain = each.find('a').get_text()
-                        user.domain_name = personal_domain
 
             if '标签信息' in basic_str:
                 basic_info = each_module.find_all(attrs={'class': 'li_1 clearfix'})
                 for each in basic_info:
                     if '标签' in each.get_text():
-                        tags = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\t', '').replace('\n\n\n',
-                                                                                                            '') \
-                            .strip().replace('\r\n', ';')
-                        user.owntag_info = tags
+                        user.tags = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\t', '').replace(
+                            '\n\n\n', '') .strip().replace('\r\n', ';')
 
             if '教育信息' in basic_str:
                 basic_info = each_module.find_all(attrs={'class': 'li_1 clearfix'})
                 for each in basic_info:
                     if '大学' in each.get_text():
-                        school_info = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\r\n', ',') \
+                        user.education_info = each.find(attrs={'class': 'pt_detail'}).get_text().replace('\r\n', ',') \
                             .replace('\t', '').replace('\n', ';').lstrip(';').rstrip(';')
-                        user.educate_info = school_info
 
             if '工作信息' in basic_str:
                 basic_info = each_module.find_all(attrs={'class': 'li_1 clearfix'})
@@ -138,8 +113,7 @@ def get_detail(html):
                         jobs = each.find_all(attrs={'class': 'pt_detail'})
                         for job in jobs:
                             jobs_info.append(job.get_text().replace('\r\n', '').replace('\t', '').replace('\n', ''))
-                all_job = ';'.join(jobs_info)
-                user.work_info = all_job
+                user.work_info = ';'.join(jobs_info)
 
             if '联系信息' in basic_str:
                 basic_info = each_module.find_all(attrs={'class': 'li_1 clearfix'})
@@ -152,9 +126,8 @@ def get_detail(html):
                         contact_info.append('email:' + each.find(attrs={'class': 'pt_detail'}).get_text())
                     if 'MSN' in each.get_text():
                         contact_info.append('msn:' + each.find(attrs={'class': 'pt_detail'}).get_text())
-                contact_str = ';'.join(contact_info)
-                user.contact_info = contact_str
+                user.contact_info = ';'.join(contact_info)
         except Exception as why:
             print('解析出错，具体原因为{why}'.format(why=why))
-        finally:
-            return user
+
+    return user
