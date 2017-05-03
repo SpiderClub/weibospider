6485305d7085a3e94f6e351a7fe22dd3364242ab
resolvewang <1796246076@qq.com>
2017-05-03 08:18:35 +0800

    增加批量插入方法

--- db/seed_ids.py
+++ db/seed_ids.py
@@ -22,4 +22,14 @@ def set_seed_crawled(uid, result):
         db_session.add(seed)
     db_session.commit()
 
+
+def get_seed_by_id(uid):
+    return db_session.query(SeedIds).filter(SeedIds.uid == uid).first()
+
+
+def insert_seeds(ids):
+    # 批量插入，遇到重复则跳过
+    db_session.execute(SeedIds.__table__.insert().prefix_with('IGNORE'), [{'uid': i} for i in ids])
+    db_session.commit()
+
 # todo 优化用户抓取逻辑，定期扫描表，看哪些用户信息已经被抓取，而他的关注和粉丝没被抓取
