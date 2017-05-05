2166211bd13d16262f09c0a1a6fa87cc969e618b
resolvewang <1796246076@qq.com>
2017-05-05 23:30:58 +0800

    给单元测试加注释

--- test_wbspider.py
+++ test_wbspider.py
@@ -2,13 +2,16 @@ import unittest
 
 
 class TestWeiboSpider(unittest.TestCase):
-    # 这单元测试写得我很尴尬...
+    # 这单元测试写得我很尴尬...ate
     def test_get_login_info(self):
         from db import login_info
         infos = login_info.get_login_info()
         self.assertEquals(len(infos), 8)
 
     def test_login(self):
+        """
+        是测试是否能成功登录的，然后登录后会把cookie保存到redis中
+        """
         import random
         from wblogin.login import get_session
         from db.login_info import get_login_info
@@ -22,6 +25,9 @@ class TestWeiboSpider(unittest.TestCase):
             raise Exception('登录失败')
 
     def test_freeze_account(self):
+        """
+        测试账号被封后是否会去查找还有可用账号没有，这里如果需要测试请换成自己数据库中的账号
+        """
         from db import login_info
         login_info.freeze_account('18708103033')
         infos = login_info.get_login_info()
@@ -30,17 +36,26 @@ class TestWeiboSpider(unittest.TestCase):
                 self.assertEqual(info.enable, 0)
 
     def test_delete_cookies(self):
+        """
+        测试根据键来删除cookie
+        """
         from db.redis_db import Cookies
         r = Cookies.delete_cookies('18708103033')
         self.assertEqual(r, True)
 
     def test_page_get(self):
+        """
+        测试页面抓取功能
+        """
         from page_get import basic
         test_url = 'http://weibo.com/p/1005051764222885/info?mod=pedit_more'
         text = basic.get_page(test_url)
         self.assertIn('深扒娱乐热点', text)
 
     def test_parse_user_info(self):
+        """
+        测试解析页面功能
+        """
         from page_parse.user import person, public
         from page_get.user import get_user_detail
         with open('./tests/writer.html') as f:
@@ -55,6 +70,9 @@ class TestWeiboSpider(unittest.TestCase):
         self.assertEqual(user.follows_num, 539)
 
     def test_get_url_from_web(self):
+        """
+        测试不同类型的用户抓取功能
+        """
         from page_get import user as user_get
         normal_user = user_get.get_profile('1195908387')
         self.assertEqual(normal_user.name, '日_推')
@@ -64,6 +82,9 @@ class TestWeiboSpider(unittest.TestCase):
         self.assertEqual(enterprise_user.level, 36)
 
     def test_get_fans(self):
+        """
+        测试用户粉丝获取功能
+        """
         from page_parse.user import public
         with open('./tests/fans.html') as f:
             cont = f.read()
@@ -73,11 +94,17 @@ class TestWeiboSpider(unittest.TestCase):
         self.assertEqual(len(cur_urls), 5)
 
     def test_bulk_insert_with_duplicates(self):
+        """
+        测试批量插入的时候是否会重复插入（请到mysql数据库中查看结果）
+        """
         from db.seed_ids import insert_seeds
         ids = ['2891529877', '2891529878', '281296709']
         insert_seeds(ids)
 
     def test_crawl_person_infos(self):
+        """
+        测试
+        """
         from tasks.user import crawl_person_infos
         crawl_person_infos('2041028560')
 
