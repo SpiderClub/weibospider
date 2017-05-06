23fc88b2d095543e7d5753eb8f8cb9b4ceac2be7
resolvewang <1796246076@qq.com>
2017-05-06 23:47:36 +0800

    添加首次用户抓取任务

--- repost_first.py
+++
@@ -1,4 +0,0 @@
-from tasks import repost
-
-if __name__ == '__main__':
-    repost.excute_repost_task()
---
+++ user_first.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+from tasks import user
+
+if __name__ == '__main__':
+    user.excute_user_task()
