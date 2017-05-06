67caeeaf77d41cd2edeb78d89d92bce7b7d6bdfc
resolvewang <1796246076@qq.com>
2017-05-06 15:19:47 +0800

    修复celery新式配置和旧式配置混用的冲突

--- tasks/workers.py
+++ tasks/workers.py
@@ -8,18 +8,17 @@ from config.conf import get_backend, get_brocker
 app = Celery('weibo_task', include=['tasks.login', 'tasks.user'], broker=get_brocker(),
              backend=get_backend())
 
-app.conf.task_queues = (
-    Queue('login_queue', exchange=Exchange('login', type='direct'), routing_key='for_login'),
-    Queue('user_crawler', exchange=Exchange('user_info', type='direct'), routing_key='for_user_info'),
-    Queue('fans_followers', exchange=Exchange('fans_followers', type='direct'), routing_key='for_fans_followers')
-)
-
 app.conf.update(
     CELERY_TIMEZONE='Asia/Shanghai',
     CELERY_ENABLE_UTC=True,
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json',
+    CELERY_QUEUES=(
+        Queue('login_queue', exchange=Exchange('login', type='direct'), routing_key='for_login'),
+        Queue('user_crawler', exchange=Exchange('user_info', type='direct'), routing_key='for_user_info'),
+        Queue('fans_followers', exchange=Exchange('fans_followers', type='direct'), routing_key='for_fans_followers')
+    )
 )
 
 
