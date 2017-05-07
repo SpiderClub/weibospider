f346ceea3ba45deda30bd183e20bec16f3a24e3d
resolvewang <1796246076@qq.com>
2017-05-07 15:46:01 +0800

    修复路由+定时器，定时任务不执行的异常

--- tasks/workers.py
+++ tasks/workers.py
@@ -10,28 +10,24 @@ app = Celery('weibo_task', include=['tasks.login', 'tasks.user'], broker=get_bro
              backend=get_backend())
 
 
-app.conf.beat_schedule = {
-    'user_task': {
-        'task': 'tasks.user.excute_user_task',
-        'schedule': timedelta(minutes=3),
-    },
-    'login_task': {
-        'task': 'tasks.login.excute_login_task',
-        'schedule': timedelta(hours=10),
-    },
-    # 'repost_task': {
-    #     'task': 'tasks.repost.excute_repost_task',
-    #     'schedule': 60*60*2.0,
-    # },
-}
-
-
 app.conf.update(
     CELERY_TIMEZONE='Asia/Shanghai',
     CELERY_ENABLE_UTC=True,
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json',
+    CELERYBEAT_SCHEDULE={
+        'user_task': {
+            'task': 'tasks.user.excute_user_task',
+            'schedule': timedelta(minutes=3),
+            'options': {'queue': 'user_crawler', 'routing_key': 'for_user_info'}
+        },
+        'login_task': {
+            'task': 'tasks.login.excute_login_task',
+            'schedule': timedelta(hours=10),
+            'options': {'queue': 'fans_followers', 'routing_key': 'for_fans_follwers'}
+        },
+    },
     CELERY_QUEUES=(
         Queue('login_queue', exchange=Exchange('login', type='direct'), routing_key='for_login'),
         Queue('user_crawler', exchange=Exchange('user_info', type='direct'), routing_key='for_user_info'),
