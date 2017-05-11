13c0b6220f009a4a30876568065d98e571b13f79
resolvewang <1796246076@qq.com>
2017-05-11 15:02:23 +0800

    修改

---
+++ search_first.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+from tasks import search
+
+if __name__ == '__main__':
+    search.excute_search_task()
--- tasks/search.py
+++ tasks/search.py
@@ -26,19 +26,17 @@ def search_keyword(keyword):
             return
 
         search_list = parse_search.get_search_info(search_page)
-        print(len(search_list))
-        if search_list:
-            # 先判断数据库里是否存在相关的微博，如果是已有的，那就说明是已经抓取的微博(因为结果默认按时间排序)，就退出循环
-            for wb_data in search_list:
-                rs = get_wb_by_mid(wb_data.weibo_id)
-                if rs:
-                    crawler.info('关键词{}本次搜索更新的微博已经获取完成'.format(keyword))
-                    return
-                else:
-                    insert_weibo_data(wb_data)
-                    # 这里暂时使用网络调用而非本地调用，权衡两种方法的好处
-                    # app.send_task('tasks.user.crawl_person_infos', args=(wb_data.uid,), queue='user_crawler',
-                    #               routing_key='for_user_info')
+        # 先判断数据库里是否存在相关的微博，如果是已有的，那就说明是已经抓取的微博(因为结果默认按时间排序)，就退出循环
+        for wb_data in search_list:
+            rs = get_wb_by_mid(wb_data.weibo_id)
+            if rs:
+                crawler.info('关键词{}本次搜索更新的微博已经获取完成'.format(keyword))
+                return
+            else:
+                insert_weibo_data(wb_data)
+                # 这里暂时使用网络调用而非本地调用，权衡两种方法的好处
+                app.send_task('tasks.user.crawl_person_infos', args=(wb_data.uid,), queue='user_crawler',
+                              routing_key='for_user_info')
 
         # 判断是否包含下一页
         if 'page next S_txt1 S_line1' in search_page:
