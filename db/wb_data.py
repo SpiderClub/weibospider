9e9da698344767d99549c30131882e1c373802f9
resolvewang <1796246076@qq.com>
2017-05-11 14:27:20 +0800

    修复批量插入的bug

--- db/wb_data.py
+++ db/wb_data.py
@@ -7,12 +7,14 @@ from decorator.decorators import db_commit_decorator
 @db_commit_decorator
 def insert_weibo_datas(weibo_datas):
     # 批量插入，遇到重复则跳过
-    db_session.execute(WeiboData.__table__.insert().prefix_with('IGNORE'), [data for data in weibo_datas])
+    dict_weibo_datas = [weibo_data.__dict__ for weibo_data in weibo_datas]
+    db_session.execute(WeiboData.__table__.insert().prefix_with('IGNORE'), dict_weibo_datas)
     db_session.commit()
 
 
 @db_commit_decorator
 def insert_weibo_data(weibo_data):
+    # 存入数据的时候从更高一层判断是否会重复，不在该层做判断
     db_session.add(weibo_data)
     db_session.commit()
 
