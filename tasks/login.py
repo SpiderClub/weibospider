182b8627a095321051e9ed40fa0722a66f9a3258
resolvewang <1796246076@qq.com>
2017-05-14 23:20:30 +0800

    增加批量调用本地登录方法

--- tasks/login.py
+++ tasks/login.py
@@ -11,6 +11,15 @@ def login_task(name, password):
     login.get_session(name, password)
 
 
+def batch_login():
+    """
+    通过本地调用相关账号进行登录，该方法可能会有用
+    """
+    infos = login_info.get_login_info()
+    for info in infos:
+        login_task(info.name, info.password)
+
+
 # worker设置并发数为1，所以可以通过sleep()限制不同账号登录速度
 @app.task(ignore_result=True)
 def excute_login_task():
@@ -20,6 +29,5 @@ def excute_login_task():
         app.send_task('tasks.login.login_task', args=(info.name, info.password), queue='login_queue',
                       routing_key='for_login')
         time.sleep(10)
-
 
 
