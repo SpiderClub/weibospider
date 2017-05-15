ad1eda1e38dcae8a1d607ebd3a1fc0ded2d9d381
resolvewang <1796246076@qq.com>
2017-05-15 16:00:13 +0800

    修改为新的解析方法

--- page_parse/comment.py
+++ page_parse/comment.py
@@ -15,6 +15,16 @@ def get_html_cont(html):
     return cont
 
 
+def get_total_page(html):
+    try:
+        page_count = json.loads(html, encoding='utf-8').get('data', '').get('page', '').get('totalpage', 1)
+    except Exception as e:
+        parser.error('获取总也面出错，具体错误是{}'.format(e))
+        page_count = 1
+
+    return page_count
+
+
 @parse_decorator(1)
 def get_next_url(html):
     """
@@ -48,18 +58,18 @@ def get_comment_list(html, wb_id):
         return list()
 
     soup = BeautifulSoup(cont, 'html.parser')
-
     comment_list = list()
-    comments = soup.find_all(attrs={'node-type': 'root_comment'})
+    comments = soup.find(attrs={'node-type': 'comment_list'}).find_all(attrs={'class': 'list_li S_line1 clearfix'})
+
     for comment in comments:
         wb_comment = WeiboComment()
         try:
             wb_comment.comment_cont = comment.find(attrs={'class': 'WB_text'}).text.strip()
             wb_comment.comment_id = comment['comment_id']
             wb_comment.user_id = comment.find(attrs={'class': 'WB_text'}).find('a').get('usercard')[3:]
+            # todo 日期格式化
             wb_comment.create_time = comment.find(attrs={'class': 'WB_from S_txt2'}).text
             wb_comment.weibo_id = wb_id
-
         except Exception as e:
             parser.error('解析评论失败，具体信息是{}'.format(e))
         else:
