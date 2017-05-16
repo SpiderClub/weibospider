cb53fab80c7e9863f914b5c5a59ec5b3bba71a86
resolvewang <1796246076@qq.com>
2017-05-16 16:47:25 +0800

    在worker中增加repost任务

--- tasks/workers.py
+++ tasks/workers.py
@@ -12,7 +12,7 @@ platforms.C_FORCE_ROOT = True
 worker_log_path = os.path.join(os.path.dirname(os.path.dirname(__file__))+'/logs', 'celery.log')
 beat_log_path = os.path.join(os.path.dirname(os.path.dirname(__file__))+'/logs', 'beat.log')
 
-tasks = ['tasks.login', 'tasks.user', 'tasks.search', 'tasks.home', 'tasks.comment']
+tasks = ['tasks.login', 'tasks.user', 'tasks.search', 'tasks.home', 'tasks.comment', 'tasks.repost']
 
 # include的作用就是注册服务化函数
 app = Celery('weibo_task', include=tasks, broker=get_broker_or_backend(1), backend=get_broker_or_backend(2))
@@ -26,6 +26,11 @@ app.conf.update(
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json',
     CELERYBEAT_SCHEDULE={
+        'login_task': {
+            'task': 'tasks.login.excute_login_task',
+            'schedule': timedelta(hours=10),
+            'options': {'queue': 'login_queue', 'routing_key': 'for_login'}
+        },
         'user_task': {
             'task': 'tasks.user.excute_user_task',
             'schedule': timedelta(minutes=3),
@@ -36,32 +41,39 @@ app.conf.update(
             'schedule': timedelta(hours=2),
             'options': {'queue': 'search_crawler', 'routing_key': 'for_search_info'}
         },
-        'login_task': {
-            'task': 'tasks.login.excute_login_task',
-            'schedule': timedelta(hours=10),
-            'options': {'queue': 'login_queue', 'routing_key': 'for_login'}
-        },
         'home_task': {
             'task': 'tasks.home.excute_home_task',
             'schedule': timedelta(hours=10),
             'options': {'queue': 'home_crawler', 'routing_key': 'home_info'}
         },
         'comment_task': {
-            'task': 'tasks.home.excute_comment_task',
+            'task': 'tasks.comment.excute_comment_task',
             'schedule': timedelta(hours=10),
-            'options': {'queue': 'home_crawler', 'routing_key': 'home_info'}
-        }
+            'options': {'queue': 'comment_crawler', 'routing_key': 'comment_info'}
+        },
+        'repost_task': {
+            'task': 'tasks.repost.excute_repost_task',
+            'schedule': timedelta(hours=10),
+            'options': {'queue': 'repost_crawler', 'routing_key': 'repost_info'}
+        },
     },
     CELERY_QUEUES=(
         Queue('login_queue', exchange=Exchange('login', type='direct'), routing_key='for_login'),
+
         Queue('user_crawler', exchange=Exchange('user_info', type='direct'), routing_key='for_user_info'),
         Queue('search_crawler', exchange=Exchange('search_info', type='direct'), routing_key='for_search_info'),
         Queue('fans_followers', exchange=Exchange('fans_followers', type='direct'), routing_key='for_fans_followers'),
+
         Queue('home_crawler', exchange=Exchange('home_crawler', type='direct'), routing_key='home_info'),
         Queue('ajax_home_crawler', exchange=Exchange('ajax_home_crawler', type='direct'), routing_key='ajax_home_info'),
+
         Queue('comment_crawler', exchange=Exchange('comment_crawler', type='direct'), routing_key='comment_info'),
         Queue('comment_page_crawler', exchange=Exchange('comment_page_crawler', type='direct'),
               routing_key='comment_page_info'),
+
+        Queue('repost_crawler', exchange=Exchange('repost_crawler', type='direct'), routing_key='repost_info'),
+        Queue('repost_page_crawler', exchange=Exchange('repost_page_crawler', type='direct'),
+              routing_key='repost_page_info'),
     )
 )
 
