0722e8572b07f1ee0280f976f6e3aa7b9e9b844c
resolvewang <1796246076@qq.com>
2017-07-02 19:37:12 +0800

    change annotations to English

--- tasks/login.py
+++ tasks/login.py
@@ -13,23 +13,14 @@ def login_task(name, password):
     login.get_session(name, password)
 
 
-def batch_login():
-    """
-    通过本地调用相关账号进行登录，该方法可能会有用
-    """
-    infos = login_info.get_login_info()
-    for info in infos:
-        login_task(info.name, info.password, info.need_verify)
-        time.sleep(10)
-
-
-# worker设置并发数为1，所以可以通过sleep()限制不同账号登录速度，账号登录速度过快，ip会被微博系统设定为异常ip
+# There should be login interval, if too many accounts login at the same time from the same ip, all the
+# accounts can be banned by weibo's anti-cheating system
 @app.task(ignore_result=True)
 def excute_login_task():
     infos = login_info.get_login_info()
-    # 每次登陆前清楚所有堆积的登录任务
+    # Clear all stacked login tasks before each time for login
     Cookies.check_login_task()
-    log.crawler.info('本轮模拟登陆开始')
+    log.crawler.info('The login task is starting...')
     for info in infos:
         app.send_task('tasks.login.login_task', args=(info.name, info.password), queue='login_queue',
                       routing_key='for_login')
