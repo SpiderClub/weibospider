3adf48c7ae305d9b4d1c20e311f83b4d147649d8
resolvewang <1796246076@qq.com>
2017-07-07 21:49:12 +0800

    change the annotation to English

--- tasks/user.py
+++ tasks/user.py
@@ -11,7 +11,7 @@ def crawl_follower_fans(uid):
         rs = user_get.get_fans_or_followers_ids(uid, 1)
         rs.extend(user_get.get_fans_or_followers_ids(uid, 2))
         datas = set(rs)
-        # 重复数据跳过插入
+        # If data already exits, just skip it
         if datas:
             insert_seeds(datas)
         set_seed_other_crawled(uid)
@@ -20,20 +20,22 @@ def crawl_follower_fans(uid):
 @app.task(ignore_result=True)
 def crawl_person_infos(uid):
     """
-    根据用户id来爬取用户相关资料和用户的关注数和粉丝数（由于微博服务端限制，默认爬取前五页，企业号的关注和粉丝也不能查看）
-    :param uid: 用户id
-    :return: 
+    Crawl user info and their fans and followers
+    For the limit of weibo's backend, we can only crawl 5 pages of the fans and followers.
+    We also have no permissions to view enterprise's followers and fans info
+    :param uid: current user id
+    :return: None
     """
     if not uid:
         return
 
-    # 由于与别的任务共享数据表，所以需要先判断数据库是否有该用户信息，再进行抓取
     user = user_get.get_profile(uid)
-    # 不抓取企业号
+    # If it's enterprise user, just skip it
     if user.verify_type == 2:
         set_seed_other_crawled(uid)
         return
 
+    # Crawl fans and followers
     app.send_task('tasks.user.crawl_follower_fans', args=(uid,), queue='fans_followers',
                   routing_key='for_fans_followers')
 
