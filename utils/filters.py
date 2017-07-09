7bbcd4ff25dd55a4f00444f94719a999528fc50e
resolvewang <1796246076@qq.com>
2017-07-09 15:27:28 +0800

    add filter for head img

--- utils/filters.py
+++ utils/filters.py
@@ -1,7 +1,7 @@
 def url_filter(url):
-    if 'sina' or 'weibo' not in url:
-        return 'http://weibo.com{}'.format(url)
-    elif 'http:' not in url:
+    if 'http' not in url:
         return 'http:{}'.format(url)
+    elif 'sina' or 'weibo' not in url:
+        return 'http://weibo.com{}'.format(url)
     else:
         return url
