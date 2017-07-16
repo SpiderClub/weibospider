f7ce363b9575cf6d999d4719153b90c79f1d74c8
resolvewang <1796246076@qq.com>
2017-07-16 20:11:30 +0800

    add rollback when select error

--- db/user.py
+++ db/user.py
@@ -1,6 +1,6 @@
 # coding:utf-8
-from db.basic_db import db_session
 from db.models import User
+from db.basic_db import db_session
 from decorators.decorator import db_commit_decorator
 
 
@@ -16,5 +16,6 @@ def save_user(user):
     db_session.commit()
 
 
+@db_commit_decorator
 def get_user_by_uid(uid):
     return db_session.query(User).filter(User.uid == uid).first()
