f58dc05a5d19188453a7f312e1e8266f01e318ad
resolvewang <1796246076@qq.com>
2017-07-16 18:37:16 +0800

    change annotations to English

--- tasks/repost.py
+++ tasks/repost.py
@@ -37,18 +37,16 @@ def crawl_repost_page(mid, uid):
 
     if total_page < limit:
         limit = total_page + 1
+
     for page_num in range(2, limit):
-        # app.send_task('tasks.comment.crawl_comment_by_page', args=(mid, page_num), queue='comment_page_crawler',
-        #               routing_key='comment_page_info')
         cur_repost_datas = crawl_repost_by_page(mid, page_num)[1]
         if cur_repost_datas:
             repost_datas.extend(cur_repost_datas)
 
-    # 补上user_id，方便可视化
     for index, repost_obj in enumerate(repost_datas):
         user_id = IdNames.fetch_uid_by_name(repost_obj.parent_user_name)
         if not user_id:
-            # 设置成根用户的uid和用户名
+            # when it comes to errors, set the args to default(root)
             repost_obj.parent_user_id = root_user.uid
             repost_obj.parent_user_name = root_user.name
         else:
@@ -60,9 +58,9 @@ def crawl_repost_page(mid, uid):
 
 @app.task(ignore_result=True)
 def excute_repost_task():
-    # 以当前微博为源微博进行分析，不向上溯源，如果有同学需要向上溯源，需要自己判断一下该微博是否是根微博
+    # regard current weibo url as the original url, you can also analyse from the root url
     weibo_datas = wb_data.get_weibo_repost_not_crawled()
-    crawler.info('本次一共有{}条微博需要抓取转发信息'.format(len(weibo_datas)))
+    crawler.info('There are {} repost urls have to be crawled'.format(len(weibo_datas)))
 
     for weibo_data in weibo_datas:
         app.send_task('tasks.repost.crawl_repost_page', args=(weibo_data.weibo_id, weibo_data.uid),
