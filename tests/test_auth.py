68a74c336b34f7dcb337c5ce4d9d70d247c38241
resolvewang <1796246076@qq.com>
2017-09-29 22:43:53 +0800

    add auth test case

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,26 @@
 import pytest
 
 
-def test_crawl_weibo_info_without_login():
-    pass
+class TestWithoutLogin:
+    def test_crawl_weibo_info_without_login(self):
+        pass
+
+    def test_crawl_weibo_comment_without_login(self):
+        pass
+
+    def test_crawl_weibo_repost_without_login(self):
+        pass
+
+    def test_get_weibo_cont_from_search_by_mid(self):
+        pass
 
 
-def test_crawl_weibo_comment_without_login():
-    pass
+class TestWithLogin:
+    def test_login(self):
+        pass
 
+    def test_crawl_user_info_with_login(self):
+        pass
 
-def test_crawl_weibo_repost_without_login():
-    pass
-
-
-def test_crawl_weibo_search_page_one_without_login():
-    pass
-
-
-def test_get_weibo_all_cont_from_search_without_login_by_mid():
-    pass
+    def test_advance_search_with_login(self):
+        pass
