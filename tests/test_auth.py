8d84860aaac49d340169ae2b54e211a1caf04c0a
resolvewang <1796246076@qq.com>
2017-09-30 21:59:04 +0800

    add test case for crawl all text

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,25 @@
 import pytest
+import requests
+
+from wblogin import get_cookies_and_headers
 
 
 class TestWithoutLogin:
-    def test_crawl_weibo_info_without_login(self):
-        pass
+    @pytest.fixture(scope='class', autouse=True)
+    def headers(self):
+        return get_cookies_and_headers()
+
+    @pytest.mark.parametrize(
+        'unique_str, url', [
+            ('$CONFIG', 'http://weibo.com/1752613937/Afa5SFjJc'),
+            ('$CONFIG', 'http://weibo.com/1319066361/Flttyxak8')
+        ])
+    def test_crawl_weibo_info_without_login(self, unique_str, url, headers):
+        resp = requests.get(url, headers=headers)
+        assert unique_str in resp.text
+
+        resp = requests.get(url)
+        assert unique_str not in resp.text
 
     def test_crawl_weibo_comment_without_login(self):
         pass
@@ -11,8 +27,14 @@ class TestWithoutLogin:
     def test_crawl_weibo_repost_without_login(self):
         pass
 
-    def test_get_weibo_cont_from_search_by_mid(self):
-        pass
+    @pytest.mark.parametrize(
+        'unique_str, url', [
+            ('html', 'http://s.weibo.com/ajax/direct/morethan140?mid=4157622578000858'),
+            ('html', 'http://s.weibo.com/ajax/direct/morethan140?mid=4157781743129391')
+        ])
+    def test_get_weibo_cont_from_search_by_mid(self, unique_str, url):
+        resp = requests.get(url)
+        assert unique_str in resp.text
 
 
 class TestWithLogin:
