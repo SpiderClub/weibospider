6025919f7c4deb1b6a9546a27b8751a2f39896b9
resolvewang <1796246076@qq.com>
2017-09-30 16:01:56 +0800

    add method for sub and subp cookies

--- wblogin/cookies_gen.py
+++ wblogin/cookies_gen.py
@@ -1,68 +1,85 @@
 import re
 import json
 import time
+import random
 
 import requests
 
+from headers import FakeChromeUA
+from exceptions import CookieGenException
 
-PASSPORT_URL = 'https://passport.weibo.com/visitor/visitor?entry=miniblog&a=' \
-               'enter&url=http://weibo.com/&domain=.weibo.com&ua=' \
-               'php-sso_sdk_client-0.6.23&_rand={}'.format(format(time.time(),
-                                                                  '0.4f'))
+
+PASSPORT_URL = 'https://passport.weibo.com/visitor/visitor?entry=miniblog&a=enter&url=http://weibo.com/&domain=.wei' \
+               'bo.com&ua=php-sso_sdk_client-0.6.23&_rand={}'.format(format(time.time(), '0.4f'))
 POST_URL = 'https://passport.weibo.com/visitor/genvisitor'
+INRARNATE_URL = 'https://passport.weibo.com/visitor/visitor?a=incarnate&t={}&w={}&c={}&gc=&cb=cross_domain&from=' \
+                'weibo&_rand={}'
+CHECK_URL = 'http://weibo.com/sinat'
 
-print(PASSPORT_URL)
+user_agent = FakeChromeUA.get_ua()
+brower_type, brower_version = user_agent.split(' ')[-2].split('/')
+brower_info = ''.join((brower_type, ','.join(brower_version.split('.'))))
 
 
-# TODO 优化这里
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    'User-Agent': user_agent,
+    'Referer': PASSPORT_URL
 }
 
-session = requests.session()
 
-
-# TODO 结合UA进行优化
-post_data = {
-    'cb': 'gen_callback',
-    'fp': '{"os":"1","browser":"Chrome61,0,3163,100","fonts":"undefined","screenInfo":"1436*752*24","plugins":"Portable Document Format::internal-pdf-viewer::Chrome PDF Plugin|::mhjfbmdgcfjbbpaeojofohoefgiehjai::Chrome PDF Viewer|::internal-nacl-plugin::Native Client|Enables Widevine licenses for playback of HTML audio/video content. (version: 1.4.8.1008)::widevinecdmadapter.dll::Widevine Content Decryption Module"}'
-}
-
-headers['Referer'] = PASSPORT_URL
-
-# resp = session.post(POST_URL, data=post_data, headers=headers)
-
-ts = 'window.gen_callback && gen_callback({"retcode":20000000,"msg":"succ","data":{"tid":"ztJRzNSeTZI4vRNqIXL2G8dnoyUfnNBEmuTPtilfVLE=","new_tid":false,"confidence":95}});'
 extract_pattern = r'({.*})'
-m = re.search(extract_pattern, ts)
-s = m.group()
-# w: True 3 False 2
-# confidence
-# tid
-gen_visitor = json.loads(s)
-tid = gen_visitor.get('data').get('tid')
-c = gen_visitor.get('data').get('confidence', 100)
-new_tid = gen_visitor.get('data').get('new_tid')
-if new_tid == 'False':
-    w = 3
-else:
-    w = 2
 
-INRARNATE_URL = 'https://passport.weibo.com/visitor/visitor?a=incarn' \
-                'ate&t={}&w={}&c={}&gc=&cb=cross_domain&from=weibo&_ran' \
-                'd={}'.format(tid, w, c, 0.09958761514503123)
 
-resp = session.get(INRARNATE_URL, headers=headers)
+def get_tid_and_c(post_url):
+    """
+    get all args including tid、c and w
+    :return: tuple(tid, c, w)
+    """
+    fp = '{' + \
+         '"os":"1","browser":"{browser}","fonts":"undefined","screenInfo":"1436*752*24","plugins":"Portable ' \
+         'Document Format::internal-pdf-viewer::Chrome PDF Plugin|::mhjfbmdgcfjbbpaeojofohoefgiehjai::Chrome PDF Viewer' \
+         '|::internal-nacl-plugin::Native Client|Enables Widevine licenses for playback of HTML audio/video content. ' \
+         '(version: 1.4.8.1008)::widevinecdmadapter.dll::Widevine Content Decryption Module"'.format(browser=brower_info) \
+         + '}'
 
-m = re.search(extract_pattern, resp.text)
-s = json.loads(m.group())
-sub = s.get('data').get('sub')
-subp = s.get('data').get('subp')
-print(sub)
-print(subp)
-print('sub: {}, subp: {}'.format(sub, subp))
-headers.update(Cookie='SUB={};SUBP={};'.format(sub, subp))
-print(headers)
-resp = session.get('http://weibo.com/u/5860464935?is_all=1', headers=headers)
-print(resp.text)
-print(session.cookies.get_dict())
+    post_data = {
+        'cb': 'gen_callback',
+        'fp': fp
+    }
+
+    resp = requests.post(post_url, data=post_data, headers=headers)
+    m = re.search(extract_pattern, resp.text)
+    try:
+        s = m.group()
+        gen_visitor = json.loads(s)
+        tid = gen_visitor.get('data').get('tid')
+        c = gen_visitor.get('data').get('confidence', 100)
+        new_tid = gen_visitor.get('data').get('new_tid')
+    except AttributeError:
+        raise CookieGenException('failed to gen cookies without login')
+    else:
+        if new_tid == 'False':
+            w = 3
+        else:
+            w = 2
+
+        return tid, c, w
+
+
+def get_sub_and_subp():
+    tid, c, w = get_tid_and_c(POST_URL)
+    inrarnate_url = INRARNATE_URL.format(tid, w, c, format(random.random(), '.17f'))
+    resp = requests.get(inrarnate_url, headers=headers)
+    try:
+        m = re.search(extract_pattern, resp.text)
+        s = json.loads(m.group())
+        sub = s.get('data').get('sub')
+        subp = s.get('data').get('subp')
+    except AttributeError:
+        raise CookieGenException('failed to gen cookies without login')
+    cookie_str = 'SUB={};SUBP={};'.format(sub, subp)
+    headers.update(Cookie=cookie_str)
+    resp = requests.get(CHECK_URL, headers=headers)
+    if '$CONFIG' not in resp.text:
+        raise CookieGenException('failed to gen cookies without login')
+    return dict(SUB=sub, SUBP=subp)
