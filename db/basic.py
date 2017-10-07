63017471cce37d5a3ef982b5d3ff5e1071d7f8a1
resolvewang <1796246076@qq.com>
2017-10-07 15:09:24 +0800

    optimize code for getting db password

--- db/basic.py
+++ db/basic.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import (
     create_engine, MetaData)
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +10,8 @@ from config import get_db_args
 
 def get_engine():
     args = get_db_args()
-    connect_str = "{}+pymysql://{}:{}@{}:{}/{}?charset=utf8".format(args['db_type'], args['user'], args['password'],
+    password = os.getenv('DB_PASS', args['password'])
+    connect_str = "{}+pymysql://{}:{}@{}:{}/{}?charset=utf8".format(args['db_type'], args['user'], password,
                                                              args['host'], args['port'], args['db_name'])
     engine = create_engine(connect_str, encoding='utf-8')
     return engine
