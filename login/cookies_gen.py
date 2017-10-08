1db268c8bce08eb6b6729e5253967e007b513c6e
resolvewang <1796246076@qq.com>
2017-10-08 14:05:38 +0800

    fix bugs for gen cookies

--- login/cookies_gen.py
+++ login/cookies_gen.py
@@ -2,14 +2,17 @@ import re
 import json
 import time
 import random
+from urllib import parse
 
 import requests
 
 from config import FakeChromeUA
+from decorators import retry
 from exceptions import CookieGenException
 
-PASSPORT_URL = 'https://passport.weibo.com/visitor/visitor?entry=miniblog&a=enter&url=http://weibo.com/&domain=.wei' \
-               'bo.com&ua=php-sso_sdk_client-0.6.23&_rand={}'.format(format(time.time(), '0.4f'))
+PASSPORT_URL = 'https://passport.weibo.com/visitor/visitor?entry=miniblog&a=enter&url={}&domain=.wei' \
+               'bo.com&ua=php-sso_sdk_client-0.6.23&_rand={}'.format(parse.quote_plus('http://weibo.com/'),
+                                                                     format(time.time(), '0.4f'))
 POST_URL = 'https://passport.weibo.com/visitor/genvisitor'
 INRARNATE_URL = 'https://passport.weibo.com/visitor/visitor?a=incarnate&t={}&w={}&c={}&gc=&cb=cross_domain&from=' \
                 'weibo&_rand={}'
@@ -18,14 +21,12 @@ CHECK_URL = 'http://weibo.com/1319066361/Flttyxak8'
 user_agent = FakeChromeUA.get_ua()
 brower_type, brower_version = user_agent.split(' ')[-2].split('/')
 brower_info = ''.join((brower_type, ','.join(brower_version.split('.'))))
-
 headers = {
     'User-Agent': user_agent,
-    'Referer': PASSPORT_URL,
+    'Referer': INRARNATE_URL,
     'Accept': '*/*',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
-    'Content-Type': 'application/x-www-form-urlencoded',
     'Host': 'passport.weibo.com',
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache'
@@ -46,12 +47,10 @@ def get_tid_and_c(post_url):
          '(version: 1.4.8.1008)::widevinecdmadapter.dll::Widevine Content Decryption Module"'.format(
              browser=brower_info) \
          + '}'
-
     post_data = {
         'cb': 'gen_callback',
         'fp': fp
     }
-
     resp = requests.post(post_url, data=post_data, headers=headers)
     m = re.search(extract_pattern, resp.text)
     try:
@@ -73,24 +72,27 @@ def get_tid_and_c(post_url):
         return tid, c, w
 
 
+@retry(10, 1, exceptions=CookieGenException)
 def get_cookies():
     """
     :return: cookies: sub and subp
     """
 
     tid, c, w = get_tid_and_c(POST_URL)
-    inrarnate_url = INRARNATE_URL.format(tid, w, c, format(random.random(), '.17f'))
+    r_tid = parse.quote_plus(tid)
+    inrarnate_url = INRARNATE_URL.format(r_tid, w, c, format(random.random(), '.17f'))
     cookies = {'tid': tid + '__' + c}
     resp = requests.get(inrarnate_url, headers=headers, cookies=cookies)
     try:
         m = re.search(extract_pattern, resp.text)
-        s = json.loads(m.group())
+        resp_str = m.group()
+        if 'errline' in resp_str:
+            raise CookieGenException('Invalid cookie without login')
+        s = json.loads(resp_str)
         sub = s.get('data').get('sub', '')
         subp = s.get('data').get('subp', '')
     except AttributeError:
         raise CookieGenException('Failed to gen cookies without login')
-    cookies = dict(SUB=sub, SUBP=subp)
-    resp = requests.get(CHECK_URL, cookies=cookies)
-    if '$CONFIG' not in resp.text:
+    if not sub and not subp:
         raise CookieGenException('Invalid cookie without login')
     return dict(SUB=sub, SUBP=subp)
