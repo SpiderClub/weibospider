9170466fb9a5836cb38131f630c66e51455fe763
resolvewang <1796246076@qq.com>
2017-10-08 16:50:34 +0800

    add request internal

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,8 @@
+import time
 import pytest
 import requests
+
+from tests import REQUEST_INTERNAL
 
 
 class TestWithoutLogin:
@@ -11,25 +14,31 @@ class TestWithoutLogin:
     def test_crawl_weibo_info_without_login(self, unique_str, url, cookies):
         resp = requests.get(url, cookies=cookies)
         assert unique_str in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
         resp = requests.get(url)
+        time.sleep(REQUEST_INTERNAL)
         assert unique_str not in resp.text
 
     def test_crawl_weibo_comment_without_login(self, cookies):
         comment_url = 'http://weibo.com/aj/v6/comment/big?ajwvr=6&id=4158045430832830&page=1'
         resp = requests.get(comment_url, cookies=cookies)
         assert 'Sina Visitor System' not in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
         resp = requests.get(comment_url)
         assert 'Sina Visitor System' in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
     def test_crawl_weibo_repost_without_login(self, cookies):
         repost_url = 'http://weibo.com/aj/v6/mblog/info/big?ajwvr=6&id=4158045430832830&page=12'
         resp = requests.get(repost_url, cookies=cookies)
         assert 'Sina Visitor System' not in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
         resp = requests.get(repost_url)
         assert 'Sina Visitor System' in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
     @pytest.mark.parametrize(
         'unique_str, url', [
@@ -39,5 +48,6 @@ class TestWithoutLogin:
     def test_get_weibo_cont_from_search_by_mid(self, unique_str, url):
         resp = requests.get(url)
         assert unique_str in resp.text
+        time.sleep(REQUEST_INTERNAL)
 
 
