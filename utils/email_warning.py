586fdbec982eaf25b5b211be6219c1f521a7ed49
resolvewang <1796246076@qq.com>
2017-10-08 15:57:30 +0800

    fix bugs for sending an email

--- utils/email_warning.py
+++ utils/email_warning.py
@@ -1,4 +1,3 @@
-import os
 from traceback import format_tb
 
 import smtplib
@@ -14,9 +13,9 @@ from logger import other
 email_args = get_email_args()
 smtp_server = email_args['server']
 smtp_port = email_args['port']
-from_addr = os.getenv('EMAIL_ACCOUNT') or email_args['from']
-from_password = os.getenv('EMAIL_PASS') or email_args['password']
-to_addr = os.getenv('EMAIL_TO') or email_args['to']
+from_addr = email_args['from']
+from_password = email_args['password']
+to_addr = email_args['to']
 email_subject = email_args['subject']
 warning_info = email_args['warning_info']
 
@@ -26,14 +25,14 @@ def _format_addr(nick_addr):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 
-def gen_msg(content, subject, from_nick=None, to_nick=None):
+def gen_msg(content, subject, email_from, email_to, from_nick=None, to_nick=None):
     if from_nick is None:
-        from_nick = from_addr
+        from_nick = email_from
     if to_nick is None:
-        to_nick = to_addr
+        to_nick = email_to
     msg = MIMEText(content, 'plain', 'utf-8')
-    msg['From'] = _format_addr('{} <{}>'.format(from_nick, from_addr))
-    msg['To'] = _format_addr('{} <{}>'.format(to_nick, to_addr))
+    msg['From'] = _format_addr('{} <{}>'.format(from_nick, email_from))
+    msg['To'] = _format_addr('{} <{}>'.format(to_nick, email_to))
     msg['Subject'] = Header(subject).encode()
     return msg
 
@@ -41,15 +40,15 @@ def gen_msg(content, subject, from_nick=None, to_nick=None):
 def send_email(email_from=from_addr, email_pass=from_password, to_addrs=None, server=smtp_server, port=smtp_port,
                from_nick='weibospider', to_nick='SpiderUser'):
     if to_addrs is None or isinstance(to_addrs, str):
-        to_addrs = [to_addr]
-    msg = gen_msg(warning_info, email_subject, from_nick, to_nick)
+        to_addrs = [to_addrs]
+    msg = gen_msg(warning_info, email_subject, email_from, to_addrs[0], from_nick, to_nick)
     server = smtplib.SMTP(server, port)
     try:
         server.starttls()
-        server.login(from_addr, email_pass)
+        server.login(email_from, email_pass)
         rs = server.sendmail(email_from, to_addrs, msg.as_string())
     except Exception as e:
-        other.error('failed to send emails, {} is raised, here are details:{}'.format(e, format_tb(e.__traceback__)[0]))
+        other.error('Failed to send emails, {} is raised, here are details:{}'.format(e, format_tb(e.__traceback__)[0]))
     else:
         return rs
     finally:
