f57f3089f32eacffd155fdb0874c5eb15cb15792
resolvewang <1796246076@qq.com>
2017-10-18 14:40:34 +0800

    add weibo data display

--- admin/weibo_data/admin.py
+++ admin/weibo_data/admin.py
@@ -1,26 +1,6 @@
 from django.contrib import admin
 
-from .models import Keywords, LoginInFo, Seeds, WbUser, WeiboData
-
-
-class KeywordsAdmin(admin.ModelAdmin):
-    list_display = ('keyword', 'enable')
-    search_fields = ['keyword']
-    list_per_page = 20
-
-
-class LoginInFoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'password', 'enable')
-    search_fields = ['name', 'enable']
-    list_per_page = 20
-
-
-class SeedsAdmin(admin.ModelAdmin):
-    list_display = (
-        'uid', 'is_crawled', 'other_crawled', 'home_crawled')
-    search_fields = ['uid']
-    list_per_page = 20
-    ordering = ['is_crawled']
+from .models import WbUser, WeiboData
 
 
 class ReadOnlyModelAdmin(admin.ModelAdmin):
@@ -56,8 +36,5 @@ class WeiboDataAdmin(ReadOnlyModelAdmin):
     list_per_page = 20
 
 
-admin.site.register(Keywords, KeywordsAdmin)
-admin.site.register(Seeds, SeedsAdmin)
-admin.site.register(LoginInFo, LoginInFoAdmin)
 admin.site.register(WbUser, WbUserAdmin)
 admin.site.register(WeiboData, WeiboDataAdmin)
--- admin/weibo_data/apps.py
+++ admin/weibo_data/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
 
-class WeiboConfig(AppConfig):
-    name = 'weibo_config'
-    verbose_name = '微博配置'
-
-
 class WeiboDataConfig(AppConfig):
     name = 'weibo_data'
     verbose_name = '微博数据'
