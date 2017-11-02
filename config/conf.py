5e160def0471c70636e36accb2f05f25275f6504
resolvewang <1796246076@qq.com>
2017-11-03 00:09:38 +0800

    add img download support

--- config/conf.py
+++ config/conf.py
@@ -104,14 +104,17 @@ def get_cookie_expire_time():
 def get_email_args():
     return cf.get('email')
 
+
 def get_images_allow():
     return cf.get('images_allow')
 
+
 def get_images_path():
-    dir = cf.get('images_path') if cf.get('images_path') else os.path.join(str(Path.home()), 'weibospider', 'images')
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir
+    img_dir = cf.get('images_path') if cf.get('images_path') else os.path.join(str(Path.home()), 'weibospider', 'images')
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    return img_dir
+
 
 def get_images_type():
     return cf.get('image_type')
