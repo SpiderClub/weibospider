1a13f0758ee1d621d775429a67bfa2dc49bf2b37
thekingofcity <1634814279@qq.com>
2018-08-19 22:57:25 +0800

    if 'noresult_tit' then return before store

--- tasks/search.py
+++ tasks/search.py
@@ -13,6 +13,8 @@ from db.dao import (
 # If you want other kind of search, you can change the url below
 # But if you change this url, maybe you have to rewrite some part of the parse code
 URL = 'http://s.weibo.com/weibo/{}&scope=ori&suball=1&page={}'
+# Use this if results are too little
+# URL = 'http://s.weibo.com/weibo/{}&nodup=1&page={}'
 LIMIT = get_max_search_page() + 1
 
 
@@ -31,8 +33,16 @@ def search_keyword(keyword, keyword_id):
 
         search_list = parse_search.get_search_info(search_page)
 
+        if cur_page == 1:
+            cur_page += 1
+        elif 'noresult_tit' not in search_page:
+            cur_page += 1
+        else:
+            crawler.info('Keyword {} has been crawled in this turn'.format(keyword))
+            return
+
         # Because the search results are sorted by time, if any result has been stored in mysql,
-        # We need not crawl the same keyword in this turn
+        # We don't need to crawl the same keyword in this turn
         for wb_data in search_list:
             rs = WbDataOper.get_wb_by_mid(wb_data.weibo_id)
             KeywordsDataOper.insert_keyword_wbid(keyword_id, wb_data.weibo_id)
@@ -45,14 +55,6 @@ def search_keyword(keyword, keyword_id):
                 # todo: only add seed ids and remove this task
                 app.send_task('tasks.user.crawl_person_infos', args=(wb_data.uid,), queue='user_crawler',
                               routing_key='for_user_info')
-        if cur_page == 1:
-            cur_page += 1
-        elif 'noresult_tit' not in search_page:
-            cur_page += 1
-        else:
-            crawler.info('Keyword {} has been crawled in this turn'.format(keyword))
-            return
-
 
 @app.task(ignore_result=True)
 def execute_search_task():
