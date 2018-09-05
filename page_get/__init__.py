ec90acef29d0fe0781d86fd881f44b35a2d70da8
thekingofcity <wzl7777777@gmail.com>
2018-09-05 11:10:50 +0800

    hot fix for cannot import name 'get_newcard_by_name'

--- page_get/__init__.py
+++ page_get/__init__.py
@@ -1,4 +1,4 @@
 from .basic import get_page
 from .status import get_cont_of_weibo
-from .user import (
-    get_profile, get_fans_or_followers_ids, get_user_profile)
+from .user import (get_profile, get_fans_or_followers_ids, get_user_profile,
+                   get_newcard_by_name)
